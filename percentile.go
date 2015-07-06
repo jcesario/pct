3a813638229948ae138c0812c2a7455a2353e2d8
John Cesario <cesario@squareup.com>
2015-07-06 00:17:05 -0700

    Commit

--- percentile.go
+++ percentile.go
@@ -3,27 +3,42 @@ package main
 import "fmt"
 import "sort"
 
-type row struct {
+type bucket struct {
 	time  float64
-	count float64
+	count int
 	total float64
 }
 
-func percentile(h []row, p int) float64 {
-	var p_ix float64
-	var cur_pctl float64
-	var total float64
-	var pctl float64
+type histogram []bucket
 
-	total = 0
-	// cur_pctl = 0
+// Implement Sort
+func (h histogram) Len() int      { return len(h) }
+func (h histogram) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+func (h histogram) Less(i, j int) bool {
+	return h[i].time < h[j].time
+}
 
-	// Find the total number of values
+// Sum the total entries in the histogram
+func qrtentries(h []bucket) int {
+	total := 0
+
 	for _, v := range h {
 		total += v.count
 	}
 
-	fmt.Printf("total: %f\n", total)
+	return total
+}
+
+func percentile(h []bucket, p int) float64 {
+	var p_ix float64
+	var cur_pctl float64
+	var total int
+	var pctl float64
+
+	// Find the total number of entries in histogram
+	total = qrtentries(h)
+
+	fmt.Printf("total: %v\n", total)
 
 	// Multiply the total number of values in the data set by the percentile, which will give you the index.
 	p_ix = (float64(total) * float64(p)) * .01
@@ -37,13 +52,13 @@ func percentile(h []row, p int) float64 {
 		times = append(times, v.time)
 	}
 
-	var count []float64
+	var count []int
 
 	for _, v := range h {
 		count = append(count, v.count)
 	}
 
-	sort.Float64s(count)
+	sort.Ints(count)
 	sort.Float64s(times)
 	fmt.Println(times)
 
@@ -61,7 +76,7 @@ func percentile(h []row, p int) float64 {
 }
 
 func main() {
-	var hist []row
+	var hist []bucket
 
 	/*
 		    Make connection to mysql
@@ -72,21 +87,21 @@ func main() {
 	*/
 	// p := [5]float32{75, 90, 95, 99, 99.9}
 
-	hist = append(hist, row{time: 85, count: 1, total: 85})
-	hist = append(hist, row{time: 34, count: 1, total: 34})
-	hist = append(hist, row{time: 42, count: 1, total: 42})
-	hist = append(hist, row{time: 51, count: 1, total: 51})
-	hist = append(hist, row{time: 84, count: 1, total: 84})
-	hist = append(hist, row{time: 86, count: 1, total: 86})
-	hist = append(hist, row{time: 78, count: 1, total: 78})
-	hist = append(hist, row{time: 85, count: 1, total: 85})
-	hist = append(hist, row{time: 87, count: 1, total: 87})
-	hist = append(hist, row{time: 69, count: 1, total: 69})
-	hist = append(hist, row{time: 94, count: 1, total: 94})
-	hist = append(hist, row{time: 74, count: 1, total: 74})
-	hist = append(hist, row{time: 65, count: 1, total: 65})
-	hist = append(hist, row{time: 56, count: 1, total: 56})
-	hist = append(hist, row{time: 97, count: 1, total: 97})
+	hist = append(hist, bucket{time: 85, count: 1, total: 85})
+	hist = append(hist, bucket{time: 34, count: 1, total: 34})
+	hist = append(hist, bucket{time: 42, count: 1, total: 42})
+	hist = append(hist, bucket{time: 51, count: 1, total: 51})
+	hist = append(hist, bucket{time: 84, count: 1, total: 84})
+	hist = append(hist, bucket{time: 86, count: 1, total: 86})
+	hist = append(hist, bucket{time: 78, count: 1, total: 78})
+	hist = append(hist, bucket{time: 85, count: 1, total: 85})
+	hist = append(hist, bucket{time: 87, count: 1, total: 87})
+	hist = append(hist, bucket{time: 69, count: 1, total: 69})
+	hist = append(hist, bucket{time: 94, count: 1, total: 94})
+	hist = append(hist, bucket{time: 74, count: 1, total: 74})
+	hist = append(hist, bucket{time: 65, count: 1, total: 65})
+	hist = append(hist, bucket{time: 56, count: 1, total: 56})
+	hist = append(hist, bucket{time: 97, count: 1, total: 97})
 	/*
 		hist = append(hist, row{time: 0.000001, count: 0, total: 0.000000})
 		hist = append(hist, row{time: 0.000003, count: 0, total: 0.000000})
@@ -146,5 +161,8 @@ func main() {
 		}
 	*/
 
-	percentile(hist, 90)
+	fmt.Printf("orig: %v\n", hist)
+	sort.Sort(histogram(hist))
+	fmt.Printf("sorted: %v\n", hist)
+	// percentile(hist, 90)
 }
